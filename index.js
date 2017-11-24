18ec4e11ab56ec93a7a6ff38fa189de2c8a06cf9
Igor Bueno <igor@alphasoftware.br>
2017-11-24 17:50:41 -0400

    Final da tarde

--- index.js
+++ index.js
@@ -2,9 +2,14 @@
 var express = require('express');
 var http = require('http');
 var routes = require('./routes');
+var bodyParser = require('body-parser');
 var app = express();
 
 //Mapeia a rota localhost:3000 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:false
+}));
 app.use(routes);
 //Cria o servidor
 http.createServer(app).listen(3000, function(){
--- routes.js
+++ routes.js
@@ -1,10 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/hello', function(req,res){
+router.get('/hello', function (req, res) {
     res.json({
-        message:'Hello world!'
+        message: 'Hello world!'
     });
 });
 
+router.get('/params/:name', function (req, res) {
+    res.json(
+        {
+            params: req.params,
+            host: req.host,
+            headers: req.headers
+        })
+});
+
+router.post('/body', function(req,res){
+     res.json(req.body);
+});
+
+router.get('/res/:name', function(req,res){
+    res.json({
+        name:req.params.name
+    });
+});
 module.exports = router;
